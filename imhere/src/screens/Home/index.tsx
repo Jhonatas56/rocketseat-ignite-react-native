97b2ff44c4455ec01c6b937f3bdf19a47ffb709a
Jhonatas J. Mendonça <118845977+Jhonatas56@users.noreply.github.com>
2024-01-13 14:22:24 -0300

    refactor: new participant is now added by participant name state

--- imhere/src/screens/Home/index.tsx
+++ imhere/src/screens/Home/index.tsx
@@ -11,16 +11,18 @@ import { Participant } from "../../components/Participant";
 import { useState } from "react";
 
 export function Home() {
-  const [participants, setParticipants] = useState(["Jhonatas"]);
+  const [participants, setParticipants] = useState<string[]>([]);
+  const [participantName, setParticipantName] = useState("");
 
   function handleParticipantAdd() {
-    if (participants.includes("Jhonatas1")) {
+    if (participants.includes(participantName)) {
       return Alert.alert(
         "Participante existe",
         "Já existe um participante com esse nome."
       );
     }
-    setParticipants((prevState) => [...prevState, "Novo participante"]);
+    setParticipants((prevState) => [...prevState, participantName]);
+    setParticipantName("");
   }
 
   function handleParticipantRemove(name: string) {
@@ -48,6 +50,8 @@ export function Home() {
           style={styles.input}
           placeholder="Nome do participante"
           placeholderTextColor="#6b6b6b"
+          onChangeText={(name) => setParticipantName(name)}
+          value={participantName}
         />
         <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
           <Text style={styles.buttonText}>+</Text>
